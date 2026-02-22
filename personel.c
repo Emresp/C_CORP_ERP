206a37aa0f7978057d13ddde18174981a928aa07
Emre Ayözcan <emreyozcan16@gmail.com>
2026-02-23 01:15:48 +0300

    feat: esnek arama ve ıd araması eklendi

--- personel.c
+++ personel.c
@@ -126,3 +126,102 @@ void personelListele()
         }
     }
 }
+
+void personelAra()
+{
+    if (personelListesininBasi==NULL)
+    {
+        printf("Kayıtlı Hiç Bir Kullanıcı Yok");
+        return;
+    }
+
+    //Esnek aramada birden fazla kişi bulabilceği için sayaç ekledim budluğu her kişinin üstüne kaçıncı kişi olduğunu yazsın diye
+    int i=1;
+
+    //Bayrak kontrolü fonksiyon kapanmadan önce bayrak hala 0 mı diye kontrol edicez bayrak hala 0 ise hiç bir şey bulunamadı anlamına gelicek
+    //Kullanıcıya bir şey bulunamadığının haberini net bir şekilde vermek için
+    int flag=0;
+
+    int arananId;
+    char arananIsim[20];
+    char arananSoyisim[20];
+    char arananDepartman[30];
+
+    printf("Aramak İstediğiniz Kişinin Bilgilerini Girin (Bilmiyorsanız \"0\" Girin)");
+
+    printf("Arancak Kişinin İD numarasını Girin\n");
+    printf("İD:");
+    scanf("%d",&arananId);
+
+    //ID numarası bize kesin sonuç verdiğinden dolayı zaten ID biliniyorsa diğer kriterleri sormaya gerek yok
+    if (arananId!=0)
+    {
+            Personel* scout=personelListesininBasi;
+
+            while (scout!=NULL)
+            {
+                if (arananId==scout->id)
+                {
+                    printf("%d.\n",i);
+                    printf("İD:%d\n",scout->id);
+                    printf("İSİM:%s\n",scout->isim);
+                    printf("SOYİSİM:%s\n",scout->soyisim);
+                    printf("DEPARTMAN:%s\n",scout->departman);
+                    printf("MAAŞ:%lf\n",scout->maas);
+                    printf("PERFORMANS:%d\n",scout->performansPuani);
+                    printf("MÜDÜR İD:%d\n",scout->bagliolduguYonetici);
+                    //Bayrak birini buldu ve değer değişti
+                    flag=1;
+                    return;
+                }
+                //İşarietçiyi bir sornaki adrese geçirdik
+                scout=scout->sonraki;
+            }
+    }
+
+    //ID numarası girilmediyse burası çalışır ve diğer kriterler sorgulanır
+    else
+    {
+        printf("Arancak Kişinin İsmini Girin\n");
+        printf("İSİM:");
+        scanf("%s",arananIsim);
+
+        printf("Arancak Kişinin Soyismini Girin\n");
+        printf("SOYİSİM:");
+        scanf("%s",arananSoyisim);
+
+        printf("Arancak Kişinin Departmanını Girin\n");
+        printf("Departman:");
+        scanf("%s",arananDepartman);
+
+        Personel* scout=personelListesininBasi;
+
+        while (scout!=NULL)
+        {
+            if ((strcmp(arananIsim, "0") == 0 || strcmp(scout->isim, arananIsim) == 0)&&
+                (strcmp(arananSoyisim, "0") == 0 || strcmp(scout->soyisim, arananSoyisim) == 0)&&
+                (strcmp(arananDepartman,"0")==0 || strcmp(scout->departman, arananDepartman) == 0))
+            {
+                printf("%d.\n",i);
+                printf("İD:%d\n",scout->id);
+                printf("İSİM:%s\n",scout->isim);
+                printf("SOYİSİM:%s\n",scout->soyisim);
+                printf("DEPARTMAN:%s\n",scout->departman);
+                printf("MAAŞ:%lf\n",scout->maas);
+                printf("PERFORMANS:%d\n",scout->performansPuani);
+                printf("MÜDÜR İD:%d\n",scout->bagliolduguYonetici);
+                i++;
+
+                //Bayrak birini buldu ve değer değişti
+                flag=1;
+
+            }
+            scout=scout->sonraki;
+        }
+    }
+    //Bayrak fonksiyon bitmesine rağmen hala 0 ise birini bulamamştır
+    if (flag==0)
+    {
+        printf("Aradığınız kriterlere uygun bir personel bulunamadı.");
+    }
+}

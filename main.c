897f12dfb3db254551bc9c67754f937a8c43be49
Emre Ayözcan <emreyozcan16@gmail.com>
2026-02-22 21:37:41 +0300

    feat: main.c icerisine interaktif kontrol paneli ve utf-8 destegi eklendi

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <windows.h>
+#include "personel.h"
 
 int main(void)
 {
-    printf("Hello, World!\n");
+
+    //Türkçe çalışmak için
+    SetConsoleOutputCP(65001); // Ekranı UTF-8
+    SetConsoleCP(65001);       // Klavyeyi de UTF-8
+
+    int secim;
+    while (1)
+    {
+        printf("SİSTEM KONTROL PANELİNE HOŞ GELDİNİZ.\n\n\n");
+        printf("Lütfen bir seçim yaparak başlayınız.\n");
+        printf("1-)Personel eklemek için\n");
+        printf("2-)Personel Listesini Görüntülemek İçin\n");
+        printf("3-)Çıkış Yapmak için\n");
+        printf("Seçim:");
+        scanf("%d",&secim);
+
+        switch (secim)
+        {
+            case 1:
+                personelEkle();
+                break;
+
+            case 2:
+                personelListele();
+                break;
+            case 3:
+                printf("Çıkış Yapılıyor");
+                return 0;
+            default:
+
+
+        }
+
+    }
+
+
     return 0;
 }

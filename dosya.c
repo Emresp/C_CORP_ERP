be53bfe84345f98bf767bf2c7d8e3ea2c7a82dea
Emre Ayözcan <emreyozcan16@gmail.com>
2026-03-12 01:01:25 +0300

    feat urunleri kalıcı kaydetmek için dosya işlemleri fonksiyonları eklendi ve boş liste kaydetme hatası giderildi.

--- dosya.c
+++ dosya.c
@@ -6,13 +6,12 @@
 
 #include <string.h>
 
+#include "urun.h"
+
 void personelleriKaydet()
 {
     //Liste zaten boşsa kayıt işlemlerine hiç başlamasın
-    if (personelListesininBasi==NULL)
-    {
-        return;
-    }
+
 
 
     Personel* scout=personelListesininBasi;
@@ -95,5 +94,97 @@ void personelleriYukle()
         }
         fclose(ykleptr);
         printf("%d Personelin bilgisi başarılı şekilde yüklendi.\n\n",i);
+    }
+}
+
+void urunleriKaydet()
+{
+
+
+    Urun* scout = urunListesininBasi;
+
+    FILE* kytptr = NULL;
+
+    //ürünler için yeni bir txt dosyası oluşturuyorum
+    kytptr = fopen("urunKayitlari.txt", "w");
+
+    if (kytptr == NULL)
+    {
+        printf("HATA: Ürün kayıt dosyası oluşturulamadı.\n");
+    }
+    else
+    {
+        while (scout != NULL)
+        {
+            fprintf(kytptr, "%d,%s,%s,%d,%.2lf,%d\n", scout->id, scout->urunadi, scout->kategori, scout->stokAdedi, scout->fiyat, scout->zimmetliPersonelid);
+
+            scout = scout->sonraki;
+        }
+
+        // Dosyayı güvenli bir şekilde kapatıyorum
+        fclose(kytptr);
+        printf("Ürün listesi basariyla urunKayitlari.txt dosyasina kaydedildi!\n");
+    }
+}
+
+void urunleriYukle()
+{
+    int i=0;
+
+    int tempId, tempStok, tempZimmetli;
+    double tempFiyat;
+    char tempUrunadi[20], tempKategori[30];
+
+    FILE* ykleptr=NULL;
+
+    // Ürün kayıtlarını okuma modunda (r) açıyorum
+    ykleptr=fopen("urunKayitlari.txt","r");
+
+    if (ykleptr==NULL)
+    {
+        // Dosya yoksa veya ilk kez açılıyorsa sorun çıkarmaması için sessizce dönüyorum
+        printf("HATA: Kayıtlı ürün dosyası okunamadı veya henüz oluşturulmadı.\n");
+        return;
+    }
+    else
+    {
+        // fscanf ile dosyadaki 6 veriyi sırasıyla ve virgüle kadar okuyup geçici değişkenlerime alıyorum
+        // 6 veriyi de başarıyla okuduğum sürece (== 6) döngüyü çalıştırıyorum
+        while (fscanf(ykleptr, "%d,%[^,],%[^,],%d,%lf,%d\n", &tempId, tempUrunadi, tempKategori, &tempStok, &tempFiyat, &tempZimmetli) == 6)
+        {
+            Urun* yeniUrun=(Urun*) malloc(sizeof(Urun));
+            yeniUrun->id=tempId;
+            strcpy(yeniUrun->urunadi,tempUrunadi);
+            strcpy(yeniUrun->kategori,tempKategori);
+            yeniUrun->stokAdedi=tempStok;
+            yeniUrun->fiyat=tempFiyat;
+            yeniUrun->zimmetliPersonelid=tempZimmetli;
+
+            // Ürünleri birbirine bağlamadan önce ramde tahsis edilen yerleri temizliyorum ki çöp değerler olmasın
+            yeniUrun->sonraki=NULL;
+            yeniUrun->onceki=NULL;
+
+            // Eğer tren boşsa, ilk vagonu yerleştiriyorum
+            if(urunListesininBasi==NULL)
+            {
+                urunListesininBasi=yeniUrun;
+            }
+            else
+            {
+                Urun* scout=urunListesininBasi;
+
+                // Trenin en sonuna kadar gidiyorum
+                while (scout->sonraki!=NULL)
+                {
+                    scout=scout->sonraki;
+                }
+                // Yeni vagonu trenin sonuna takıyorum
+                scout->sonraki = yeniUrun;
+                yeniUrun->onceki = scout;
+            }
+            i++; // Yüklenen ürün sayısını artırıyorum
+        }
+        fclose(ykleptr);
+        printf("%d Ürünün bilgisi başarılı şekilde yüklendi.\n\n",i);
     }
 }
